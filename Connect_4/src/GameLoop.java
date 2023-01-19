1a751517d9aab42725e19cb5049e82575e8776b6
makirsa <122559942+makirsa@users.noreply.github.com>
2023-01-18 23:46:59 -0500

    Add files via upload

---
+++ Connect_4/src/GameLoop.java
@@ -0,0 +1,11 @@
+public class GameLoop {
+
+    private final Connect4Game game;
+    private final GUI ourGUI;
+
+    public GameLoop() {
+        game = new Connect4Game("G", "O", 6, 7);
+        ourGUI = new GUI(game.isIs1playing(), game, 6, 7);
+    }
+
+}
